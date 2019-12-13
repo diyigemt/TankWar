1d5087f2640506420d5c21bf118f15bb073d8b3d
diyigemt <qwe1355247243@gmail.com>
2019-12-13 17:41:55 +0800

    完成地图映射

--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -42,7 +42,7 @@ public class Constants {
     public static final String WINDOW_TITLE = "TankWar";
 
     //地图坐标平移
-    public static final float MAP_TRANSLATION_X = 10.0f;
+    public static final float MAP_TRANSLATION_X = -2.5f;
     public static final float MAP_TRANSLATION_Y = 0.0f;
 
     //方向
