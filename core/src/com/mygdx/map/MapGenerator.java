f3f482660397e407eebb20c3750bfb63ca65a765
diyigemt <qwe1355247243@gmail.com>
2019-12-13 21:11:10 +0800

    地图创建完成一半

--- core/src/com/mygdx/map/MapGenerator.java
+++ core/src/com/mygdx/map/MapGenerator.java
@@ -15,21 +15,21 @@ public class MapGenerator {
 	public void generate(int mapId) {
 		String divide[] = Gdx.files.internal("map/" + String.valueOf(mapId) + ".txt").readString().split("\\r\\n");
 		for (int i = 0; i < 4; i++) {
-			if (divide[0].charAt(0) == '0')
+			if (divide[0].equals("0"))
 				continue;
 			WallType type = WallType.getInstance(i);
 			String position[] = divide[i].split("'");
-			for (int j = 1; j <= Integer.valueOf(position[0]); j++) {
+			for (int j = 1; j < position.length; j++) {
 				String point[] = position[j].split(",");
 				switch (point[0].charAt(0)) {
-					case '0' : createAWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]));
-					case '1' : createABigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]));
-					case '2' : createAListSmallWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3]));
-					case '3' : createAListSmallWall(type, Float.valueOf(point[2]) , Float.valueOf(point[1]), Float.valueOf(point[3]));
-					case '4' : createAListBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3]));
-					case '5' : createAListBigWall(type, Float.valueOf(point[2]) , Float.valueOf(point[1]), Float.valueOf(point[3]));
-					case '6' : createAListSmallBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3]));
-					case '7' : createAListSmallBigWall(type, Float.valueOf(point[2]) , Float.valueOf(point[1]), Float.valueOf(point[3]));
+					case '0' : createAWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2])); break;
+					case '1' : createABigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2])); break;
+					case '2' : createAListVSmallWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
+					case '3' : createAListHSmallWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
+					case '4' : createAListVBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
+					case '5' : createAListHBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
+					case '6' : createAListVSmallBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
+					case '7' : createAListHSmallBigWall(type, Float.valueOf(point[1]) , Float.valueOf(point[2]), Float.valueOf(point[3])); break;
 				}
 			}
 		}
@@ -41,24 +41,42 @@ public class MapGenerator {
 	}
 	//创建一个由4个小墙组成的大墙
 	private void createABigWall(WallType type, float x, float y) {
-		createAListSmallWall(type, x, y, y + Constants.WALL_SIZE);
-		createAListSmallWall(type, x + Constants.WALL_SIZE, y, y + Constants.WALL_SIZE);
+		createAListVSmallWall(type, x, y, y + Constants.WALL_SIZE);
+		createAListVSmallWall(type, x + Constants.WALL_SIZE, y, y + Constants.WALL_SIZE);
 	}
 	//创建一列小墙
-	private void createAListSmallWall(WallType type, float x, float y, float end) {
-		for (;y <= end; y += Constants.WALL_SIZE) {
+	private void createAListVSmallWall(WallType type, float x, float y, float endY) {
+		for (;y <= endY; y += Constants.WALL_SIZE) {
+			createAWall(type, x, y);
+		}
+	}
+	//创建一行小墙
+	private void createAListHSmallWall(WallType type, float x, float y, float endX) {
+		for (;x <= endX; x += Constants.WALL_SIZE) {
 			createAWall(type, x, y);
 		}
 	}
 	//创建一列大墙
-	private void createAListBigWall(WallType type, float x, float y, float end) {
-		for (;y <= end; y += Constants.WALL_SIZE * 2) {
+	private void createAListVBigWall(WallType type, float x, float y, float endY) {
+		for (;y <= endY; y += Constants.WALL_SIZE * 2) {
+			createAWall(type, x, y);
+		}
+	}
+	//创建一行大墙
+	private void createAListHBigWall(WallType type, float x, float y, float endX) {
+		for (;x <= endX; x += Constants.WALL_SIZE * 2) {
 			createAWall(type, x, y);
 		}
 	}
 	//创建一列由4个小墙组成的大墙
-	private void createAListSmallBigWall(WallType type, float x, float y, float end) {
-		for (; y <= end; y+= Constants.WALL_SIZE * 2) {
+	private void createAListVSmallBigWall(WallType type, float x, float y, float endY) {
+		for (; y <= endY; y+= Constants.WALL_SIZE * 2) {
+			createABigWall(type, x, y);
+		}
+	}
+	//创建一行由4个小墙组成的大墙
+	private void createAListHSmallBigWall(WallType type, float x, float y, float endX) {
+		for (; x <= endX; x+= Constants.WALL_SIZE * 2) {
 			createABigWall(type, x, y);
 		}
 	}
